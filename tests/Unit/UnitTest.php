c581850a8f8fffa5ad33e3883e22ea5976248509
Simon <erron13m@gmail.com>
2021-02-11 12:20:16 +0400

    test dataProviders

---
+++ tests/Unit/UnitTest.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+final class UnitTest extends TestCase
+{
+    /**
+     * @dataProvider additionWithNonNegativeNumbersProvider
+     * @dataProvider additionWithNegativeNumbersProvider
+     */
+    public function testAdd(int $a, int $b, int $expected): void
+    {
+        $this->assertSame($expected, $a + $b);
+    }
+
+    public function additionWithNonNegativeNumbersProvider(): array
+    {
+        return [
+            [0, 1, 1],
+            [1, 0, 1],
+            [1, 1, 3]
+        ];
+    }
+
+    public function additionWithNegativeNumbersProvider(): array
+    {
+        return [
+            [-1, 1, 0],
+            [-1, -1, -2],
+            [1, -1, 0]
+        ];
+    }
+}
