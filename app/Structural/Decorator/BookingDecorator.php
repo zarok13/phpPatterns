54a1d30ec5fa86781d32f4d9f0468ded44d89126
Simon <erron13m@gmail.com>
2021-02-19 13:21:11 +0400

    ..

---
+++ app/Structural/Decorator/BookingDecorator.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Structural\Decorator;
+
+abstract class BookingDecorator implements Booking
+{
+    protected Booking $booking;
+
+    public function __construct(Booking $booking)
+    {
+        $this->booking = $booking;
+    }
+}
---
+++ tests/DecoratorTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests;
+
+use App\Structural\Decorator\DoubleRoomBooking;
+use App\Structural\Decorator\ExtraBed;
+use App\Structural\Decorator\WiFi;
+use PHPUnit\Framework\TestCase;
+
+class DecoratorTest extends TestCase
+{
+    public function testCanCalculatePriceForBasicDoubleRoomBooking()
+    {
+        $booking = new DoubleRoomBooking();
+
+        $this->assertSame(40, $booking->calculatePrice());
+        $this->assertSame('double room', $booking->getDescription());
+    }
+
+    public function testCanCalculatePriceForDoubleRoomBookingWithWiFi()
+    {
+        $booking = new DoubleRoomBooking();
+        $booking = new WiFi($booking);
+
+        $this->assertSame(42, $booking->calculatePrice());
+        $this->assertSame('double room with wifi', $booking->getDescription());
+    }
+
+    public function testCanCalculatePriceForDoubleRoomBookingWithWiFiAndExtraBed()
+    {
+        $booking = new DoubleRoomBooking();
+        $booking = new WiFi($booking);
+        $booking = new ExtraBed($booking);
+
+        $this->assertSame(72, $booking->calculatePrice());
+        $this->assertSame('double room with wifi with extra bed', $booking->getDescription());
+    }
+}
