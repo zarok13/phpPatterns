761e2595ad53b5ac934941c2a3a08efeac8e94d7
zarok13 <erron13m@gmail.com>
2021-02-22 09:40:37 +0400

    DataMapper

---
+++ tests/DataMapperTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests;
+
+use InvalidArgumentException;
+use App\Structural\DataMapper\StorageAdapter;
+use App\Structural\DataMapper\User;
+use App\Structural\DataMapper\UserMapper;
+use PHPUnit\Framework\TestCase;
+
+class DataMapperTest extends TestCase
+{
+    public function testCanMapUserFromStorage()
+    {
+        $storage = new StorageAdapter([1 => ['username' => 'domnikl', 'email' => 'liebler.dominik@gmail.com']]);
+        $mapper = new UserMapper($storage);
+
+        $user = $mapper->findById(1);
+
+        $this->assertInstanceOf(User::class, $user);
+    }
+
+    public function testWillNotMapInvalidData()
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        $storage = new StorageAdapter([]);
+        $mapper = new UserMapper($storage);
+
+        $mapper->findById(1);
+    }
+}
