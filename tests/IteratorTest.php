b26610df801083eccdc52da01d6e44f6056cb47f
Simon <erron13m@gmail.com>
2021-02-24 13:48:17 +0400

    Iterator finished

---
+++ tests/IteratorTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Behavioral\Iterator\Tests;
+
+use App\Behavioral\Iterator\Book;
+use App\Behavioral\Iterator\BookList;
+use PHPUnit\Framework\TestCase;
+
+require "app/Behavioral/Iterator/Examples/Conceptual.php";
+
+class IteratorTest extends TestCase
+{
+    public function testCanIterateOverBookList()
+    {
+        $bookList = new BookList();
+        $bookList->addBook(new Book('Learning PHP Design Patterns', 'William Sanders'));
+        $bookList->addBook(new Book('Professional Php Design Patterns', 'Aaron Saray'));
+        $bookList->addBook(new Book('Clean Code', 'Robert C. Martin'));
+
+        $books = [];
+
+        foreach ($bookList as $book) {
+            $books[] = $book->getAuthorAndTitle();
+        }
+
+        $this->assertSame(
+            [
+                'Learning PHP Design Patterns by William Sanders',
+                'Professional Php Design Patterns by Aaron Saray',
+                'Clean Code by Robert C. Martin',
+            ],
+            $books
+        );
+    }
+
+    public function testCanIterateOverBookListAfterRemovingBook()
+    {
+        $book = new Book('Clean Code', 'Robert C. Martin');
+        $book2 = new Book('Professional Php Design Patterns', 'Aaron Saray');
+
+        $bookList = new BookList();
+        $bookList->addBook($book);
+        $bookList->addBook($book2);
+        $bookList->removeBook($book);
+
+        $books = [];
+        foreach ($bookList as $book) {
+            $books[] = $book->getAuthorAndTitle();
+        }
+
+        $this->assertSame(
+            ['Professional Php Design Patterns by Aaron Saray'],
+            $books
+        );
+    }
+
+    public function testCanAddBookToList()
+    {
+        $book = new Book('Clean Code', 'Robert C. Martin');
+
+        $bookList = new BookList();
+        $bookList->addBook($book);
+
+        $this->assertCount(1, $bookList);
+    }
+
+    public function testCanRemoveBookFromList()
+    {
+        $book = new Book('Clean Code', 'Robert C. Martin');
+
+        $bookList = new BookList();
+        $bookList->addBook($book);
+        $bookList->removeBook($book);
+
+        $this->assertCount(0, $bookList);
+    }
+}
